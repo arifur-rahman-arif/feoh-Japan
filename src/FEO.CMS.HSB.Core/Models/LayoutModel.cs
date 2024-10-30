70fe97dfb2df2fbe0bf04277ebb9557298461eed
Anjani Achanta <anjani@vertis.digital>
2024-10-30 18:56:37 +0530

    Run time errors fixed during solution setup.

--- src/FEO.CMS.HSB.Core/Models/LayoutModel.cs
+++ src/FEO.CMS.HSB.Core/Models/LayoutModel.cs
@@ -6,9 +6,5 @@ namespace FEO.CMS.HBG.Core.Models
     {
         public ContentArea Header { get; set; }
         public ContentArea Footer { get; set; }
-        public bool HideHeader { get; set; }
-        public bool HideFooter { get; set; }
-        public bool HideBreadcrumb { get; set; }
-        public bool ShowInBreadcrumb { get; set; }
     }
 }
