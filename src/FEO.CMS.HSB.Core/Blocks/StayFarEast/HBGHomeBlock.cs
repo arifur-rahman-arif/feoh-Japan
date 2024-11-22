2ec6e0390dbcc68ab56fa865700122dae913c82b
carlcortina <vertis_digital@LAPTOP-RSS7HNIJ>
2024-11-22 11:14:56 +0800

    https://vertisdigital.atlassian.net/browse/FEM-509

---
+++ src/FEO.CMS.HSB.Core/Blocks/StayFarEast/HBGHomeBlock.cs
@@ -0,0 +1,10 @@
+﻿using EPiServer.DataAnnotations;
+
+namespace FEO.CMS.HBG.Core.Blocks.StayFarEast
+{
+    //Home Rendering Settings
+    [ContentType(DisplayName = "HBGHomeBlock", GUID = "92324923-8464-4077-8015-77610E877E4B", GroupName = "Hospitality")]
+    public class HBGHomeBlock : HBGHomeBannerConfigBlock
+    {
+    }
+}
---
+++ src/FEO.CMS.HSB.Core/Helpers/BlockHelper.cs
@@ -0,0 +1,16 @@
+﻿using EPiServer.Core;
+using EPiServer.Web.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FEO.CMS.HBG.Core.Helpers
+{
+    public static class BlockHelper
+    {
+        public static T GetCurrentPage<T>() where T : PageData
+        {
+            var pageRouteHelper = EPiServer.ServiceLocation.ServiceLocator.Current.GetService<IPageRouteHelper>();
+            var CurrentPage = pageRouteHelper?.Page as T;
+            return CurrentPage;
+        }
+    }
+}
