eeb522ab308b49b71ac17cbc2b1a1d928f7c0139
carlcortina <vertis_digital@LAPTOP-RSS7HNIJ>
2024-11-26 13:22:24 +0800

    FEM-503

---
+++ src/FEO.CMS.HSB.Core/Blocks/StayFarEast/HBGDestinationsBlock.cs
@@ -0,0 +1,15 @@
+﻿using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+
+namespace FEO.CMS.HBG.Core.Blocks.StayFarEast
+{
+    [ContentType(DisplayName = "HBGDestinationsBlock", GUID = "C5FA2907-B0AC-45AB-9392-0BC5CB5644F3", GroupName = "Hospitality")]
+    public class HBGDestinationsBlock : HBGBaseBlock
+    {
+        [Display(Name = "Datasource", GroupName = SystemTabNames.Content, Order = 100)]
+        [CultureSpecific]
+        public virtual ContentReference Datasource { get; set; }
+    }
+}
