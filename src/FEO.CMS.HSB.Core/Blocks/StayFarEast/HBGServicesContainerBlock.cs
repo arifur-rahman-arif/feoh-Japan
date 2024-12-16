0e101302719a906c2f5a0fbad9c1875730d64cd4
Praneeth Mallam <praneeth@vertis.digital>
2024-12-16 16:26:17 +0530

    Created ServicesContainerBlock and POIContainerBlock.

---
+++ src/FEO.CMS.HSB.Core/Blocks/StayFarEast/HBGServicesContainerBlock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EPiServer.DataAnnotations;
+
+namespace FEO.CMS.HBG.Core.Blocks.StayFarEast
+{
+    [ContentType(DisplayName = "HBGServicesContainerBlock", GroupName = "Hospitality")]
+    public class HBGServicesContainerBlock
+    {
+    }
+}
