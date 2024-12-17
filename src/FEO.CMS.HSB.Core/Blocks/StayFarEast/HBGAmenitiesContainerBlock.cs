2a110d3f1e4e8d40cf9e912185cf4d662a3a3e5f
Praneeth Mallam <praneeth@vertis.digital>
2024-12-17 19:24:31 +0530

    Created AmenitiesContainerBlock and created controller for MeetingLandingPage and PackageLandingPage

--- src/FEO.CMS.HSB.Core/Blocks/StayFarEast/HBGAmenitiesContainerBlock.cs
+++ src/FEO.CMS.HSB.Core/Blocks/StayFarEast/HBGAmenitiesContainerBlock.cs
@@ -6,11 +6,14 @@ using System.ComponentModel.DataAnnotations;
 namespace FEO.CMS.HBG.Core.Blocks.StayFarEast
 {
     [ContentType(DisplayName = "HBGAmenitiesContainerBlock", GUID = "FC8CB9EE-469C-4C13-86C5-2B913CA08D80", GroupName = "Hospitality")]
-    public class HBGAmenitiesContainerBlock : HBGBaseBlock
+    public class HBGAmenitiesContainerBlock : HBGAmenitiesContainerConfigBlock
     {
         [Display(Name = "Featured Amenities", GroupName = SystemTabNames.Content, Order = 100)]
         [CultureSpecific]
         public virtual ContentArea FeaturedAmenities { get; set; }
 
+        [Display(Name = "All Amenities", GroupName = SystemTabNames.Content, Order = 200)]
+        [CultureSpecific]
+        public virtual ContentReference AllAmenities { get; set; }
     }
 }
--- src/FEO.CMS.HSB.Core/Blocks/StayFarEast/HBGAmenitiesContainerConfigBlock.cs
+++ src/FEO.CMS.HSB.Core/Blocks/StayFarEast/HBGAmenitiesContainerConfigBlock.cs
@@ -1,11 +1,12 @@
 using EPiServer.DataAbstraction;
 using EPiServer.DataAnnotations;
+using FEO.CMS.HBG.Core.Blocks.Bases;
 using System.ComponentModel.DataAnnotations;
 
 namespace FEO.CMS.HBG.Core.Blocks.StayFarEast
 {
     [ContentType(DisplayName = "HBGAmenitiesContainerConfigBlock", GUID = "5181A49C-AE79-4BEF-81C9-D3DFA1127567", GroupName = "Hospitality")]
-    public class HBGAmenitiesContainerConfigBlock : HBGBaseBlock
+    public class HBGAmenitiesContainerConfigBlock : HBGRootBlock
     {
         [Display(Name = "Gallery List", GroupName = SystemTabNames.Settings, Order = 100)]
         [CultureSpecific]
