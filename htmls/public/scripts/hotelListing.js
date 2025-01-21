ddda0fbef5dc7e7329ecacecc05add07699865d1
aminul-vertis <aminul@vertis.digital>
2025-01-21 18:47:08 +0600

    FEM-1747 added show all script

--- htmls/public/scripts/hotelListing.js
+++ htmls/public/scripts/hotelListing.js
@@ -13,3 +13,28 @@ new Swiper('.swiper-container', {
         clickable: true
     }
 });
+
+/* Show All Function Starts*/
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hotels = document.querySelectorAll('.hotel-row');
+    const showAllButton = document.querySelector('.show-all-button-container');
+
+    hotels.forEach((hotel, index) => {
+        if (index >= 3) {
+            hotel.style.display = 'none';
+            console.log(`Hiding hotel row at index ${index}`);
+        }
+    });
+
+    // Add click event listener to the "Show All" button
+    showAllButton?.addEventListener('click', () => {
+        hotels.forEach(hotel => {
+            hotel.style.display = 'flex'; // Show all hotels
+        });
+
+        showAllButton.style.display = 'none'; // Hide the button
+    });
+});
+
+/* Show All Function Ends*/
