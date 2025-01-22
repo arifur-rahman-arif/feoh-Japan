7972155936442441e9913625fea86be33933d07a
AR. Arif <arif@vertis.digital>
2025-01-22 11:04:17 +0600

    FEM-1743 added navigation to the masthead slider

--- htmls/public/scripts/masthead.js
+++ htmls/public/scripts/masthead.js
@@ -1,4 +1,5 @@
-// import Swiper from "swiper"; While working enable this import to have type checking
+// While working enable this import to have type checking
+// import Swiper from 'swiper';
 
 new Swiper('.masthead-swiper', {
     autoplay: {
@@ -7,6 +8,10 @@ new Swiper('.masthead-swiper', {
     },
     speed: 2000,
     slidesPerView: 1,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
     pagination: {
         el: '.swiper-pagination'
     }
